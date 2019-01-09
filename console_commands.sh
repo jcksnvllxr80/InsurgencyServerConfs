a8bfe8291e37dfa9a3f01fd280ff3b3e474a7b43
jcksnvllxr80 <ac.watkins80@gmail.com>
2019-01-09 08:44:31 -0500

    initial commit

---
+++ console_commands.sh
@@ -0,0 +1,1 @@
+./Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping Oilfield?Scenario=Scenario_Refinery_Checkpoint_Insurgents?MaxPlayers=8?Password=r6 -multihome=@SERVERIP -Port=@SERVERPORT -QueryPort=@SERVERPORT1 -RconPassword=1413854 -RconListenPort=@SERVERPORT2 -log -hostname="GamingNerdsCoop" -MapCycle=MyMapCycle.txt
